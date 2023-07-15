db3746dd1168532f44af877701742d219d0153a0
JaySonani <jsonani98@gmail.com>
2023-07-15 02:58:14 -0400

    Search student page complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <header className="bg-gray-900 text-white py-4">
       <nav className="container mx-auto flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Find My University</h1>
+        <h1 className="text-2xl font-bold cursor-pointer" onClick={() => {
+          window.location = '/'
+        }} >Find My University</h1>
         <div>
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 mr-2 rounded">
             Login
--- src/pages/search-students/index.js
+++ src/pages/search-students/index.js
@@ -1,6 +1,5 @@
-import { Box, Grid, TextField } from "@mui/material";
+import { Box, Grid, Button, TextField, Select, MenuItem, InputLabel, FormControl } from "@mui/material";
 import Mentor from "../../components/Mentor";
-import SearchFilterDropdown from "../../components/SearchFilterDropdown";
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
@@ -9,6 +8,10 @@ const SearchStudents = () => {
 
     const [mentors, setMentors] = useState([]);
     const [searchText, setSearchText] = useState("")
+
+    const [selectedCourse, setSelectedCourse] = useState("")
+    const [selectedUniversity, setSelectedUniversity] = useState("")
+    const [filteredData, setFilteredData] = useState([])
 
     useEffect(() => {
         fetchData();
@@ -19,17 +22,55 @@ const SearchStudents = () => {
             const response = await axios.get('https://ec30-70-50-195-236.ngrok-free.app/searchstudents');
             console.log("API Response:")
             setMentors(response.data);
+            setFilteredData(response.data)
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     }
 
     const listOfUniversities = ["University of ABC", "University of PQR"]
-    const listofCourses = ["MACS", "MEng"]
+    const listofCourses = ["Computer Science", "Business Administration"]
 
-    const filteredData = mentors.filter((item) =>
-        item.first_name.toLowerCase().includes(searchText.toLowerCase())
-    );
+    const handleFilterData = (searchText, selectedCourse, selectedUniversity) => {
+
+        const tempData = mentors.filter((item) => {
+            if (searchText) return item.first_name.toLowerCase().includes(searchText.toLowerCase())
+            else return item
+        }).filter((item) => {
+            if (selectedCourse) return item.course_name === selectedCourse
+            else return item
+        }).filter((item) => {
+            if (selectedUniversity) return item.university_name === selectedUniversity
+            else return item
+        })
+
+        setFilteredData(tempData)
+    }
+
+    const handleCourseChange = (event) => {
+        setSelectedCourse(event.target.value)
+        handleFilterData(searchText, event.target.value, selectedUniversity);
+    }
+
+    const handleUniversityChange = (event) => {
+        setSelectedUniversity(event.target.value)
+        handleFilterData(searchText, selectedCourse, event.target.value);
+
+    }
+
+    const handleSearchTextChange = (event) => {
+        setSearchText(event.target.value)
+        handleFilterData(event.target.value, selectedCourse, selectedUniversity);
+    }
+
+    const clearFilters = () => {
+        setSearchText('')
+        setSelectedCourse('')
+        setSelectedUniversity('')
+        setFilteredData(mentors)
+    }
+
+
 
     return (
         <div>
@@ -38,28 +79,64 @@ const SearchStudents = () => {
                     <Grid container direction="row" spacing={3} >
                         <Grid item md={8}>
                             <Box sx={{ width: "100%" }}>
-                                <TextField placeholder="Search by name" value={searchText} onChange={(e) => { setSearchText(e.target.value) }} fullWidth />
+                                <TextField placeholder="Search by name" value={searchText} onChange={handleSearchTextChange} fullWidth />
                             </Box>
                         </Grid>
                         <Grid item flex={2}>
-                            <SearchFilterDropdown filterLabel={'Select University'} availableFilters={listOfUniversities} />
+                            <FormControl fullWidth>
+                                <InputLabel>{'Select Course name'}</InputLabel>
+                                <Select
+                                    value={selectedCourse}
+                                    label={'Select Course name'}
+                                    onChange={handleCourseChange}
+                                    fullWidth>
+                                    {
+                                        listofCourses.map((course) => {
+                                            return <MenuItem value={course}>{course}</MenuItem>
+                                        })
+                                    }
+                                </Select>
+                            </FormControl>
                         </Grid>
                         <Grid item flex={2}>
-                            <SearchFilterDropdown filterLabel={'Select Course name'} availableFilters={listofCourses} />
+                            <FormControl fullWidth>
+                                <InputLabel>{'Select University'}</InputLabel>
+                                <Select
+                                    value={selectedUniversity}
+                                    label={'Select University'}
+                                    onChange={handleUniversityChange}
+                                    fullWidth>
+                                    {
+                                        listOfUniversities.map((university) => {
+                                            return <MenuItem value={university}>{university}</MenuItem>
+                                        })
+                                    }
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item flex={1}>
+                            <Button sx={{height:'56px'}} onClick={clearFilters}  variant='contained'>{'CLEAR'}</Button>
+
                         </Grid>
                     </Grid>
                 </Box>
 
                 <Grid item container spacing={5} className="grid" justifyContent="center">
+
                     {
+                        filteredData.length > 0 &&
+
                         filteredData.map((mentor) => {
                             return (
                                 <Grid item>
                                     <Mentor mentor={mentor} />
-
                                 </Grid>
                             )
                         })
+
+                    }
+                    {
+                        filteredData.length === 0 && <Box sx={{ my: 5 }}>No mentors found</Box>
                     }
                 </Grid>
             </Box>
